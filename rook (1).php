40c867cd093ece995e7f88bebfdb3b888a047613
omar-bad <real25130@gmail.com>
2017-02-18 22:36:27 +0300

    REAL-TEAM

---
+++ rook (1).php
@@ -0,0 +1,268 @@
+<?php
+$botToken = "300049013:AAH_MhPC3CyxQUvLF2LR37u3eSbMRa5ZkYE"; // توكن
+$website = "https://api.telegram.org/bot".$botToken;
+$sudo_id = 325384922; // ايدي المطور
+$bot_id = 300049013; // ايدي البوت 
+//include ("omar.php");
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+$chatId = $update["message"]["chat"]["id"];
+$nm = $update["message"]["new_chat_member"];
+$type = $update["message"]["chat"]["type"];
+$user = $update["message"]["from"]["username"];
+$name = $update["message"]["chat"]["title"];
+$message = $update["message"]["text"];
+$mse = $update["message"]["text"];
+$for = $update["message"]["from"]["id"];
+$nam = $update["message"]["from"]["first_name"];
+$sticker = $update["message"]["sticker"];
+$photo = $update["message"]["photo"];
+$audio = $update["message"]["voice"];
+$link = $update["message"]["[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/"];
+$fwd = $update["message"]["forward_from"];
+$fwd2 = $update["message"]["forward_from"]["id"];
+$fwd3 = $update["message"]["forward_to"];
+$user2 = $update["message"]["forward_from"]["username"];
+$pin = $update["message"]["pinned_message"];
+$gif = $update["message"]["document"];
+$ed = $update["message"]["edited_channel_post"];
+$nt = $update["message"]["new_chat_title"];
+$np = $update["message"]["new_chat_photo"];
+$dp = $update["message"]["delete_chat_photo"];
+$nm = $update["message"]["new_chat_member"];
+$left = $update["message"]["left_chat_member"];
+$test = $update["message"]["contact"];
+$song = $update["message"]["audio"];
+$location = $update["message"]["location"];
+$memb = $update["message"]["message_id"];
+$game = $update["message"]["game"]; 
+$reply = $update["message"]["reply_to_message"];
+$replay = $update["message"]["reply_to_message"]["from"]["id"];
+$replay_user = $update["message"]["reply_to_message"]["from"]["username"];
+$user_id = $update['message']['from']['id'];
+$text = $update['message']['text'];
+$token =$botToken ;
+$text = $update['message']['text'];
+$mensagemID = $update['message']['reply_to_message']['message_id'];
+$chatID = $update['message']['reply_to_message']['chat']['id'];
+$fwdrep = $update['message']['reply_to_message']['forward_from']['id'];
+$mensagemID = $update['message']['reply_to_message']['message_id'];
+$group = -1001055207438; // ايدي الكروب لاستقبال الرسائل
+
+
+if ($message == "/help"){
+	sendMessage($chatId, "اهلا 💡بك عزيزي" . " @" . $user . "\n
+	
+	كيفية 🗒 استخدام البوت 🤖🍃
+	\n\n
+	1~ اضف البوت لاي مجموعة وقم بجعله ادمن سوف يبدأ تلقائيا في التحذير 
+	\n
+	2~ لاضهار الايدي الخاص بك اكتب id/
+	\n
+	3~ لاضهار ايدي عضو بل قم بل رد عليه بل امر id
+	\n
+	4~ قم بتحويل رسالة من عضو الى البوت للحصول على معلوماته
+	\n
+	5~ اكتب الوقت لمعرفة الوقت 
+	\n
+	6~ اكتب التاريخ لمعرفت التاريخ 
+	\n
+	7~ ارسل رسالة للبوت سيتم تحوليها الى المطور ليتم الرد عليك 
+	\n
+	8~ لجعل البوت يكلم العضو قم بل رد على العضو وقم بكتابة كلة + الكلام سيقوم البوت بكتابت ما طلبت
+	\n
+	9~ اكتب هينة بل رد على شخص لاهانة عضو معين
+	\n\n
+	مطور البوت 💡 @Omar_Real
+	" 
+	);
+}
+if ($message && $chatId != $group && $chatId != -1001111061467){
+	forwardMessage($group ,$chatId, $memb);
+//	sendMessage($chatId, "TEST", $mensagens['message']['message_id'],$mensagemID , true);
+}
+
+
+if ($message && $fwdrep){
+sendMessage($fwdrep, " $message " );	
+}
+
+if ($message){
+sendMessage($for, $fwd3);	
+}
+
+$shit = explode(".", $message);
+
+
+
+if($text == 'hi'){
+	
+sendMessage($chatId,"$text_reply");
+}
+if($text == 'هلو'){
+sendMessage($chatId, "Hello");
+}
+
+
+if ($shit[0] == "كلة" && $reply ){
+	sendMessage($chatId, "$shit[1] " . "@" . $replay_user);
+}
+
+
+if ($replay && $message == "هينة" && $for == $sudo_id){
+	sendMessage($chatId, "دي لك زبالة 🌚😹 " . "@" . $replay_user);
+}
+
+$matches = explode(".", $message); // Group id and msg / ايدي المجموعة او القناة + الرسالة سيرسلها البووت 
+if($message){
+sendMessage($matches[0], "$matches[1]");
+}
+
+if($fwd2 && $bot_id){
+sendMessage($for, "💡Id : " . $fwd2 . "\n💡user : " . "@" . $user2);	
+}
+
+if ($replay && $message == "id"){
+sendMessage($chatId, "💡Id : " . $replay . "\n💡User : " . "@" . $replay_user);
+}
+
+if ($nm){
+sendMessage($chatId, "🔥اهلا عزيزي \n💡تابع @set_web ");
+}
+
+if($message == "/me" and $for == $sudo_id){
+sendMessage($chatId, "انت المطور مال اني محح " . "@" . $user);
+}
+
+if($message == "/me" and $for != $sudo_id){
+sendMessage($chatId, "انت عضو مستفرخ دي " . "@" . $user);
+}
+
+if($location and $for != $sudo_id){
+sendMessage($chatId, "لا ترسل موقعك يا خرا " . "@" . $user);
+}
+
+if($game and $for != $sudo_id){
+sendMessage($chatId, " لا تلعب العاب يا خرا " . "@" . $user);
+}
+
+if($song and $for != $sudo_id){
+sendMessage($chatId, "لا ترسل اغاني يا خرا " . "@" . $user);
+}
+
+if($message == "نوع المجموعة"){
+sendMessage ($chatId, "نوع المجموعة هوة : " . $type); 
+}
+
+if($message == "عدد رسائلي"){
+sendMessage ($chatId, "عدد رسائلك هوة : " . $memb); 
+}
+
+if($dp and $for != $sudo_id){
+sendMessage($chatId, "هاذ الخرا وخر صورت الكروب " . "@" . $user);
+}
+
+if($np){
+sendMessage($chatId, "تم تغير صورت الكروب بواسطت  " . "@" . $user);
+}
+
+if($nt){
+sendMessage($chatId, "قام بتغيير الاسم " . "@" . $user);
+}
+
+if($ed){
+sendMessage($chatId, "لا  تعدل يا خرا " . "@" . $user); // By OmarReal
+}
+
+if($gif and $for != $sudo_id){
+sendMessage($chatId, "لا ترسل صور متحركة يا خرا " . "@" . $user);
+}
+
+if($pin and $for != $sudo_id){
+sendMessage($chatId, "لا تثبت يا خرا " . "@" . $user);
+}
+
+
+
+if($fwd && !$photo and $for != $sudo_id){
+sendMessage($chatId, "لا تسوي توجيه يا خرا " . "@" . $user);
+}
+
+
+if($link and $for != $sudo_id){
+sendMessage($chatId, "لا ترسل روابط يا خرا " . "@" . $user);
+}
+
+if($audio and $for != $sudo_id){
+sendMessage($chatId, "لا ترسل بصمات يا خرا " . "@" . $user);
+}
+
+if($photo and $for != $sudo_id){
+sendMessage($chatId, "لا ترسل صور يا خرا " . "@" . $user);
+}
+
+if($test and $for != $sudo_id){
+sendMessage($chatId, "  لا ترسل جهة اتصال يخرا " . "@" . $user);
+}
+
+if ($left){
+sendMessage($chatId, " هذا لخرا طلع   " . "@" . $user);
+}
+
+if ($sticker and $for != $sudo_id){
+sendMessage($chatId, "لا ترسل ملصقات يا خرا " . "@" . $user); // OmarReal
+}
+
+if ($message == "/start"){
+	sendMessage($chatId, "اهلا بك 💡 بك يا" .  " @" . $user ." " . "اضفني 💭 الى مجموعتك 👥 وسوف اقوم بل تحذير 📵\n✨ قناة السورس @set_web" );
+}
+
+// code by omar
+
+if ($message === "/id" && $message != $replay){
+	sendMessage ($chatId, "🎈 Gp Id : " . $chatId 
+	. "\n" . "🎈 User : " 
+	. "@"  . $user 
+	. "\n" 
+	. "🎈 Gp name : " . $name
+	
+	. "\n" . "🎈 Msg text : " . $mse
+	. "\n" . "🎈 Your Id : " . $for
+	. "\n" . "🎈 Msg Number : " . $memb
+	. "\n" . "🎈 Type : " . $type
+	. "\n" . "🎈 Your Name : " . $nam
+	);
+}
+
+// This File By @Omar_Real
+/*
+if ($message == "/id"){
+	sendMessage($chatId, "اهلا 👋 يا @" . $user . "\n" . "لقدم تم ارسال 📩 طلبك في الخاص 💡\n تفقد الخاص ارسل 📪 رسالة للبوت اذا لم تتلقى شيئا 💸");
+}
+*/
+$time = time() + (979 * 11 + 1 + 30);
+if ($message ==  'الوقت' ){
+sendMessage($chatId, "🕛 البلد : العراق" . "\n" . "🕛 الساعة : " . date('g', $time) . "\n" . "🕛 الدقيقة : " . date('i', $time));
+}
+
+if ($message == "التاريخ"){
+sendMessage($chatId, "📆 البلد : العراق \n" . "📆  السنة : " . date("Y") . "\n" . "📆 الشهر : " . date("n") . "\n" . "📆 اليوم :" . date("j"));	
+}
+date_default_timezone_set("Asia/Baghdad");
+
+
+	function forwardMessage ($group, $chatId, $memb){
+		   $url = $GLOBALS[website].'/forwardMessage?chat_id='.$group.'&from_chat_id='.$chatId.'&message_id='.$memb;
+			file_get_contents($url);
+		}
+
+
+	function sendMessage ($chatId, $message){
+		
+
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+		file_get_contents($url);
+		}
+
+		?>
+		
